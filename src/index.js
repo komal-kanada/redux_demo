98ccc8b59eae261c5815b79d8bb4720d570bb930
komal-kanada <komal.kanda@bacancytechnology.com>
2019-05-10 12:14:32 +0530

    Apply redux devtool

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware,compose } from "redux";
 import reducer from "./store/reducers/reducer";
 import thunk from "redux-thunk";
 
@@ -21,8 +21,8 @@ import thunk from "redux-thunk";
 // };
 
 // const store = createStore(reducer,applyMiddleware(logAction));
-
-const store = createStore(reducer,applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer ,composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
